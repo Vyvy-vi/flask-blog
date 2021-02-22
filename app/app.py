dc65c617d97c37d182da8d652c45f503baa510d0
Vyvy-vi <vyom.j@protonmail.com>
2021-02-22 20:23:42 +0530

    feat: added sqlalchemy *and broke the project

--- app/app.py
+++ app/app.py
@@ -1,25 +1,75 @@
 import sqlite3
 from flask import Flask, render_template, request, url_for, flash, redirect
 from werkzeug.exceptions import abort
+
 from flask import Markup
+from flask_admin import Admin, AdminIndexView
+from flask_admin.contrib.sqla import ModelView
+
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import UserMixin, LoginManager, current_user, login_user, logout_user
 
 def get_db_connection():
     conn = sqlite3.connect('database.db')
     conn.row_factory = sqlite3.Row
     return conn
 
-
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'yrtfbi3h495utih'
+app.config['SECRET_KEY'] = 'Hi3d6f45a5fc12445dbac2f59c3b6c7cb1'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 
-@app.route('/')
+db = SQLAlchemy(app)
+login = LoginManager(app)
+
+@login.user_loader
+def load_user(user_id):
+    return User.query.get(user_id)
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50))
+
+
+class _ModelView(ModelView):
+    def is_accessible(self):
+        return current_user.is_authenticated
+
+    def inaccessible_callback(self, name, **kwargs):
+        return redirect(url_for('login'))
+
+
+class _AdminIndexView(AdminIndexView):
+    def is_accessible(self):
+        return current_user.is_authenticated
+
+    def inaccessible_callback(self, name, **kwargs):
+        return redirect(url_for('login'))
+
+
+app.config['FLASK_ADMIN_SWATCH'] = 'cerulean'
+admin = Admin(app, name='Blog-Admin', template_mode='bootstrap3', index_view=_AdminIndexView())
+admin.add_view(_ModelView(User, db.session))
+
+@app.route('/login')
+def login():
+    user = User.query.get(1)
+    login_user(user)
+    return "Logged in!"
+@app.route('/logout')
+def logout():
+    logout_user()
+    return "Logged out!"
+
+"""
+app.route('/')
 def index():
     conn = get_db_connection()
     posts = conn.execute('SELECT * FROM posts').fetchall()
     conn.close()
     return render_template('index.html', posts=posts)
+"""
 
-
+"""
 def get_post(post_id):
     conn = get_db_connection()
     post = conn.execute('SELECT * FROM posts WHERE id = ?',
@@ -28,7 +78,6 @@ def get_post(post_id):
     if post is None:
         abort(404)
     return post
-
 
 @app.route('/<int:post_id>')
 def post(post_id):
@@ -87,3 +136,8 @@ def delete(id):
     conn.close()
     flash('"{}" was successfully deleted!'.format(post['title']))
     return redirect(url_for('index'))
+"""
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
